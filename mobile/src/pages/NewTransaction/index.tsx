99f631352d8bd05c70db9ab5fee1aef90a9dc229
leandrosouzaa <leandror.dev@gmail.com>
2020-05-12 12:22:34 -0300

    create pages

---
+++ mobile/src/pages/NewTransaction/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+import {Container} from './styles';
+
+const NewTransaction: React.FC = () => {
+   return (
+      <Container>
+         <Text>NewTransaction</Text>
+      </Container>
+   );
+};
+
+export default NewTransaction;
---
+++ mobile/src/pages/index.ts
@@ -0,0 +1,4 @@
+import Dashboard from './Dashboard';
+import NewTransaction from './NewTransaction';
+
+export {Dashboard, NewTransaction};
