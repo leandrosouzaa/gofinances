f6cf1ae870524f0fd92a37544da0580870c62ad6
leandrosouzaa <leandror.dev@gmail.com>
2020-06-08 11:03:09 -0300

    delete old menu

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 import {Text} from 'react-native';
 
-import Menu from '../../components/Menu';
-
 import {Container} from './styles';
 
 const Dashboard: React.FC = () => {
    return (
       <Container>
          <Text>Dashboard</Text>
-         <Menu />
       </Container>
    );
 };
--- mobile/src/pages/NewTransaction/index.tsx
+++ mobile/src/pages/NewTransaction/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-import Menu from '../../components/Menu';
-
 import {Container} from './styles';
 
 const NewTransaction: React.FC = () => {
    return (
       <Container>
          <Text>NewTransaction</Text>
-         <Menu />
       </Container>
    );
 };
