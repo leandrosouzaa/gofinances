a516b064f519a692f2182c9f2af5de475949ca67
leandrosouzaa <leandror.dev@gmail.com>
2020-06-08 12:12:50 -0300

    create Header component

---
+++ mobile/src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import React, {useMemo} from 'react';
+
+import {format} from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import {Container, Logo, TextDate} from './styles';
+import logo from '../../assets/images/logo.png';
+
+const Header: React.FC = () => {
+   const today = useMemo(() => {
+      return format(new Date(), "dd 'de' MMMM", {locale: ptBR});
+   }, []);
+
+   return (
+      <Container>
+         <Logo source={logo} />
+         <TextDate>{today}</TextDate>
+      </Container>
+   );
+};
+
+export default Header;
---
+++ mobile/src/components/Header/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+   background-color: #5636d3;
+   height: 81px;
+   flex-direction: row;
+   justify-content: space-around;
+   align-items: center;
+`;
+
+export const Logo = styled.Image`
+   width: 170px;
+`;
+
+export const TextDate = styled.Text`
+   font-size: 14px;
+   color: #fff;
+   opacity: 0.6;
+   font-family: 'Poppins-Regular';
+`;
