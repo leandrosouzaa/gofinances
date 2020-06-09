f079cd38bde2f19bc0ef668ad77c8b139ffd88e5
leandrosouzaa <leandror.dev@gmail.com>
2020-06-09 11:28:19 -0300

    change style props

--- mobile/src/components/Card/styles.ts
+++ mobile/src/components/Card/styles.ts
@@ -1,4 +1,4 @@
-import styled, {css} from 'styled-components/native';
+import styled from 'styled-components/native';
 
 interface CardProps {
    backgroundColor: string;
@@ -15,48 +15,29 @@ export const Container = styled.View<CardProps>`
    justify-content: space-around;
    padding: 18px 0 42px 0;
    margin-right: 16px;
-   ${(props) =>
-      props.backgroundColor && css`background-color: {props.backgroundColor}`};
+   background-color: ${(props) => props.backgroundColor};
 `;
 
 export const Info = styled.View``;
 
 export const Type = styled.Text<ThemeProps>`
-   color: #363f5f;
+   color: ${(props) => (props.colorTheme === '#fff' ? '#363F5F' : '#fff')};
    font-family: 'Poppins-Regular';
    font-size: 14px;
    line-height: 21px;
-
-   ${(props) =>
-      props.colorTheme !== '#fff' &&
-      css`
-         color: #fff;
-      `}
 `;
 
 export const Value = styled.Text<ThemeProps>`
    font-size: 30px;
-   color: #363f5f;
+   color: ${(props) => (props.colorTheme === '#fff' ? '#363F5F' : '#fff')};
    font-family: 'Poppins-Regular';
    line-height: 45px;
    margin-top: 45px;
-
-   ${(props) =>
-      props.colorTheme !== '#fff' &&
-      css`
-         color: #fff;
-      `}
 `;
 
 export const Last = styled.Text<ThemeProps>`
    font-family: 'Poppins-Regular';
    font-size: 12px;
    line-height: 18px;
-   color: #969cb3;
-
-   ${(props) =>
-      props.colorTheme !== '#fff' &&
-      css`
-         color: #fff;
-      `}
+   color: ${(props) => (props.colorTheme === '#fff' ? '#363F5F' : '#fff')};
 `;
