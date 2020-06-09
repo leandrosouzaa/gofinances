0af8008a8d353bc5faadcbf7304cf31cf37ba30d
leandrosouzaa <leandror.dev@gmail.com>
2020-06-09 20:11:58 -0300

    page structure (initial)

--- mobile/src/pages/NewTransaction/index.tsx
+++ mobile/src/pages/NewTransaction/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import {Header} from '../../components';
+
 import {Container} from './styles';
 
 const NewTransaction: React.FC = () => {
    return (
-      <Container>
-         <Text>NewTransaction</Text>
-      </Container>
+      <>
+         <Header />
+         <Container>
+            <Text>NewTransaction</Text>
+         </Container>
+      </>
    );
 };
 
