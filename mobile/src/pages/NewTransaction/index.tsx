b7264727c3bdc54a996273af21e5f6e8dcddbcc3
leandrosouzaa <leandror.dev@gmail.com>
2020-06-10 13:32:23 -0300

    new transaction screen done

--- mobile/src/pages/NewTransaction/index.tsx
+++ mobile/src/pages/NewTransaction/index.tsx
@@ -1,76 +1,132 @@
-import React, {useRef, useCallback} from 'react';
-import {TextInput} from 'react-native';
+import React, {useRef, useCallback, useState} from 'react';
 
+import Icon from 'react-native-vector-icons/Feather';
+import {
+   TextInput,
+   KeyboardAvoidingView,
+   ScrollView,
+   Platform,
+} from 'react-native';
 import {Form} from '@unform/mobile';
 import {FormHandles} from '@unform/core';
 
 import {Header, Input, Button} from '../../components';
 
-import {Container, Title} from './styles';
+import {
+   Container,
+   Title,
+   SelectView,
+   SelectItem,
+   SelectItemText,
+   SelectItemButton,
+} from './styles';
 
 const NewTransaction: React.FC = () => {
    const formRef = useRef<FormHandles>(null);
    const valueInputRef = useRef<TextInput>(null);
    const categoryInputRef = useRef<TextInput>(null);
+   const [isSelected, setIsSelected] = useState<boolean>(true);
+   const [type, setType] = useState<'outcome' | 'income'>('income');
 
    const handleAddTransaction = useCallback(() => {}, []);
 
+   const handleSelectType = (bool: boolean, value: 'outcome' | 'income') => {
+      if (bool === isSelected) {
+         return;
+      }
+      setType(value);
+      setIsSelected(!isSelected);
+   };
+
    return (
-      <>
-         <Header />
-         <Container>
-            <Form
-               ref={formRef}
-               onSubmit={() => {
-                  handleAddTransaction;
-               }}>
-               <Title>Cadastro</Title>
+      <KeyboardAvoidingView
+         style={{flex: 1}}
+         enabled
+         behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+         <ScrollView
+            keyboardShouldPersistTaps="handled"
+            contentContainerStyle={{flex: 1}}>
+            <Header />
+            <Container>
+               <Form
+                  ref={formRef}
+                  onSubmit={() => {
+                     handleAddTransaction;
+                  }}>
+                  <Title>Cadastro</Title>
 
-               <Input
-                  name="title"
-                  autoCorrect={false}
-                  autoCapitalize="none"
-                  keyboardType="default"
-                  icon="align-center"
-                  placeholder="Título da Transação"
-                  returnKeyType="next"
-                  onSubmitEditing={() => {
-                     valueInputRef.current?.focus();
-                  }}
-               />
+                  <Input
+                     name="title"
+                     autoCorrect={false}
+                     autoCapitalize="none"
+                     keyboardType="default"
+                     icon="align-center"
+                     placeholder="Título da Transação"
+                     returnKeyType="next"
+                     onSubmitEditing={() => {
+                        valueInputRef.current?.focus();
+                     }}
+                  />
 
-               <Input
-                  ref={valueInputRef}
-                  name="value"
-                  autoCorrect={false}
-                  autoCapitalize="none"
-                  keyboardType="number-pad"
-                  icon="dollar-sign"
-                  placeholder="Preço"
-                  returnKeyType="next"
-                  onSubmitEditing={() => {
-                     categoryInputRef.current?.focus();
-                  }}
-               />
+                  <Input
+                     ref={valueInputRef}
+                     name="value"
+                     autoCorrect={false}
+                     autoCapitalize="none"
+                     keyboardType="number-pad"
+                     icon="dollar-sign"
+                     placeholder="Preço"
+                     returnKeyType="next"
+                     onSubmitEditing={() => {
+                        categoryInputRef.current?.focus();
+                     }}
+                  />
 
-               <Input
-                  ref={categoryInputRef}
-                  name="category"
-                  autoCorrect={false}
-                  autoCapitalize="words"
-                  keyboardType="default"
-                  icon="paperclip"
-                  placeholder="Categoria"
-                  returnKeyType="next"
-                  onSubmitEditing={() => {
-                     formRef.current?.submitForm();
-                  }}
-               />
+                  <SelectView>
+                     <SelectItem value={type} isSelected={isSelected}>
+                        <SelectItemButton
+                           onPress={() => handleSelectType(true, 'income')}>
+                           <Icon
+                              name="arrow-up-circle"
+                              size={20}
+                              color="#12A454"
+                           />
+                           <SelectItemText>Entrada</SelectItemText>
+                        </SelectItemButton>
+                     </SelectItem>
 
-               <Button text="Cadastrar" />
-            </Form>
-         </Container>
-      </>
+                     <SelectItem value={type} isSelected={!isSelected}>
+                        <SelectItemButton
+                           onPress={() => handleSelectType(false, 'outcome')}>
+                           <Icon
+                              name="arrow-down-circle"
+                              size={20}
+                              color="#E83F5B"
+                           />
+                           <SelectItemText>Saída</SelectItemText>
+                        </SelectItemButton>
+                     </SelectItem>
+                  </SelectView>
+
+                  <Input
+                     ref={categoryInputRef}
+                     name="category"
+                     autoCorrect={false}
+                     autoCapitalize="words"
+                     keyboardType="default"
+                     icon="paperclip"
+                     placeholder="Categoria"
+                     returnKeyType="next"
+                     onSubmitEditing={() => {
+                        formRef.current?.submitForm();
+                     }}
+                  />
+
+                  <Button text="Cadastrar" />
+               </Form>
+            </Container>
+         </ScrollView>
+      </KeyboardAvoidingView>
    );
 };
 
--- mobile/src/pages/NewTransaction/styles.ts
+++ mobile/src/pages/NewTransaction/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components/native';
 
+interface SelectItemProps {
+   isSelected: boolean;
+   value: 'income' | 'outcome';
+}
+
 export const Container = styled.View`
    flex: 1;
    background-color: #f0f2f5;
@@ -11,4 +16,39 @@ export const Title = styled.Text`
    line-height: 30px;
    font-family: 'Poppins-Regular';
    margin-bottom: 24px;
+`;
+
+export const SelectView = styled.View`
+   width: 100%;
+   justify-content: space-between;
+   flex-direction: row;
+   height: 50px;
+   margin-bottom: 16px;
+`;
+
+export const SelectItem = styled.View<SelectItemProps>`
+   border: ${(props) => (props.isSelected ? 0 : '1.5px solid #969cb3')};
+   width: 49%;
+   border-radius: 5px;
+   background-color: ${(props) =>
+      props.value === 'income' && props.isSelected
+         ? 'rgba(18, 164, 84, 0.1)'
+         : props.value === 'outcome' && props.isSelected
+         ? 'rgba(232, 63, 91, 0.1)'
+         : 0};
+`;
+
+export const SelectItemButton = styled.TouchableOpacity`
+   justify-content: center;
+   align-items: center;
+   flex-direction: row;
+   flex: 1;
+   background-color: transparent;
+`;
+
+export const SelectItemText = styled.Text`
+   color: #363f5f;
+   font-family: 'Poppins-Regular';
+   font-size: 14px;
+   margin-left: 16px;
 `;
