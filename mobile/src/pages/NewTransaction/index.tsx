cdf8381f68f8686c17ec8dc5bf03c896aa9a86e9
leandrosouzaa <leandror.dev@gmail.com>
2020-06-10 10:28:37 -0300

    use inputs

--- mobile/src/pages/NewTransaction/index.tsx
+++ mobile/src/pages/NewTransaction/index.tsx
@@ -1,16 +1,72 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useRef, useCallback} from 'react';
+import {TextInput} from 'react-native';
 
-import {Header} from '../../components';
+import {Form} from '@unform/mobile';
+import {FormHandles} from '@unform/core';
 
-import {Container} from './styles';
+import {Header, Input} from '../../components';
+
+import {Container, Title} from './styles';
 
 const NewTransaction: React.FC = () => {
+   const formRef = useRef<FormHandles>(null);
+   const valueInputRef = useRef<TextInput>(null);
+   const categoryInputRef = useRef<TextInput>(null);
+
+   const handleAddTransaction = useCallback(() => {}, []);
+
    return (
       <>
          <Header />
          <Container>
-            <Text>NewTransaction</Text>
+            <Form
+               ref={formRef}
+               onSubmit={() => {
+                  handleAddTransaction;
+               }}>
+               <Title>Cadastro</Title>
+
+               <Input
+                  name="title"
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  keyboardType="default"
+                  icon="align-center"
+                  placeholder="Título da Transação"
+                  returnKeyType="next"
+                  onSubmitEditing={() => {
+                     valueInputRef.current?.focus();
+                  }}
+               />
+
+               <Input
+                  ref={valueInputRef}
+                  name="value"
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  keyboardType="number-pad"
+                  icon="dollar-sign"
+                  placeholder="Preço"
+                  returnKeyType="next"
+                  onSubmitEditing={() => {
+                     categoryInputRef.current?.focus();
+                  }}
+               />
+
+               <Input
+                  ref={categoryInputRef}
+                  name="category"
+                  autoCorrect={false}
+                  autoCapitalize="words"
+                  keyboardType="default"
+                  icon="paperclip"
+                  placeholder="Categoria"
+                  returnKeyType="next"
+                  onSubmitEditing={() => {
+                     formRef.current?.submitForm();
+                  }}
+               />
+            </Form>
          </Container>
       </>
    );
--- mobile/src/pages/NewTransaction/styles.ts
+++ mobile/src/pages/NewTransaction/styles.ts
@@ -2,5 +2,13 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
    flex: 1;
-   background-color: #e5e5e5;
+   background-color: #f0f2f5;
+   padding: 24px 24px 0px 24px;
+`;
+
+export const Title = styled.Text`
+   font-size: 20px;
+   line-height: 30px;
+   font-family: 'Poppins-Regular';
+   margin-bottom: 24px;
 `;
