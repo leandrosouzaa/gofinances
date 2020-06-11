92370f12652909fcec00d15dafbebb7941019a95
leandrosouzaa <leandror.dev@gmail.com>
2020-06-11 12:05:21 -0300

    web

---
+++ web/src/pages/Dashboard/index.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+
+import api from '../../services/api';
+
+import Header from '../../components/Header';
+
+import formatValue from '../../utils/formatValue';
+
+import { Container, CardContainer, Card, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+const Dashboard: React.FC = () => {
+  moment.locale('pt-BR');
+
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const { data } = await api.get('/transactions');
+
+      setTransactions(data.transactions);
+      setBalance(data.balance);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">{formatValue(balance.income)}</h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">
+              {formatValue(balance.outcome)}
+            </h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">{formatValue(balance.total)}</h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(t => (
+                <tr key={t.id}>
+                  <td className="title">{t.title}</td>
+                  <td className={t.type}>
+                    {t.type === 'outcome' && '- '}
+                    {formatValue(t.value)}
+                  </td>
+                  <td>{t.category.title}</td>
+                  <td>{moment(t.created_at).format('L')}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ web/src/pages/Import/index.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable array-callback-return */
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import Header from '../../components/Header';
+import FileList from '../../components/FileList';
+import Upload from '../../components/Upload';
+
+import { Container, Title, ImportFileContainer, Footer } from './styles';
+
+import alert from '../../assets/alert.svg';
+import api from '../../services/api';
+
+interface FileProps {
+  file: File;
+  name: string;
+  readableSize: string;
+}
+
+const Import: React.FC = () => {
+  const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
+  const history = useHistory();
+
+  async function handleUpload(): Promise<void> {
+    await Promise.all(
+      uploadedFiles.map(uFile => {
+        const data = new FormData();
+        data.append('file', uFile.file);
+
+        try {
+          api.post('/transactions/import', data);
+        } catch (err) {
+          console.log(err.error.message);
+        }
+      }),
+    );
+
+    history.goBack();
+  }
+
+  function submitFile(files: File[]): void {
+    files.map(f => {
+      const file: FileProps = {
+        file: f,
+        name: f.name,
+        readableSize: String(f.size),
+      };
+
+      setUploadedFiles([...uploadedFiles, file]);
+    });
+  }
+
+  return (
+    <>
+      <Header size="small" />
+      <Container>
+        <Title>Importar uma transação</Title>
+        <ImportFileContainer>
+          <Upload onUpload={submitFile} />
+          {!!uploadedFiles.length && <FileList files={uploadedFiles} />}
+
+          <Footer>
+            <p>
+              <img src={alert} alt="Alert" />
+              Permitido apenas arquivos CSV
+            </p>
+            <button onClick={handleUpload} type="button">
+              Enviar
+            </button>
+          </Footer>
+        </ImportFileContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Import;
